991c4f8485eac893e1b20e2502632e645eb28d8a
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2025-09-13 13:25:47 +0200

    test init uv

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,7 @@
-from fastmcp import FastMCP
+import fastmcp
 
-mcp = FastMCP("Demo 🚀")
-
-@mcp.tool
-def greet():
-    """Just present yourself basically"""
-    return "Hello my name is Mistral and I am a helpful AI assistant"
+def main():
+    print("Hello World!")
 
 if __name__ == "__main__":
-    mcp.run()
+    main()
